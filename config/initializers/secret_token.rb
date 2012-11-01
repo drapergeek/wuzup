0fe88d4e92be5e1143533084f23946e3c1448341
Jason Draper <jason@drapergeek.com>
2012-11-01 19:26:54 -0400

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wuzup::Application.config.secret_token = '499754000435b31e64046ad0e769a1f3442771808f4976df1e4b26de24470f21db5b3f041e61556e76435c5454f5495b6338ed84876ea13ecb23e03d1deb6b1e'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Wuzup::Application.routes.draw do
+  root :to => 'Clearance::Sessions#new'
+
+end
